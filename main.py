5635c4886d9e306628534e0f5ead329432b679f3
VictorHenrique01 <vic10souz@gmail.com>
2025-04-03 21:43:50 -0300

    clean code em main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from autenticacao import login_requerido
 from routes.aluno_routes import alunos_blueprint
 from routes.professor_routes import professores_blueprint
 from routes.turma_routes import turmas_blueprint
-
 from autenticacao import login_blueprint
 
 app = Flask(__name__)
@@ -19,8 +18,7 @@ dicie = {
     ],
 }
 
-#ROTAS
-#resetar
+# ROTA PRA RESETAR TD
 @app.route("/reseta", methods=['POST'])
 @login_requerido
 def resetar_professor():
@@ -28,111 +26,15 @@ def resetar_professor():
     dicie["professores"] = []
     dicie["turma"] = []
     return jsonify({"mensagem": "Dados resetados com sucesso!"}), 200
-#PROFESSOR
-# exibir professor
-@app.route('/professores', methods=['GET'])
-@login_requerido
-def listar_professores():
-    return jsonify({
-        "mensagem": "Bem-vindo à página de professores!",
-        "turma": dicie["turma"]
-    }), 200
-    
 
-#exibir professor pelo id
-@app.route('/professores/<int:id_professor>', methods=['GET'])
-def obter_professor(id_professor):
-    for professor in dicie["professores"]:
-        if professor["id"] == id_professor:
-            return jsonify(professor)
-    return jsonify({"error": "Professor não encontrado"}), 404
-
-#criar um professor
-@app.route('/professores', methods=['POST'])
-def criar_professor():
-    dados = request.json
-    novo_professor = {"id": dados["id"], "nome": dados["nome"]}
-    dicie["professores"].append(novo_professor)
-    return jsonify(novo_professor), 201
-
-#atualizar um professor
-@app.route('/professores/<int:id_professor>', methods=['PUT'])
-def atualizar_professor(id_professor):
-    for professor in dicie["professores"]:
-        if professor["id"] == id_professor:
-            dados = request.json
-            nome_atualizado = dados.get("nome")
-            if nome_atualizado:
-                professor["nome"] = nome_atualizado
-            return jsonify({"message": "Professor atualizado com sucesso", "professor": professor})
-    return jsonify({"error": "Professor não encontrado"}), 404
-
-#deletar um professor
-@app.route('/professores/<int:id_professor>', methods=['DELETE'])
-def excluir_professor(id_professor):
-    for professor in dicie["professores"]:
-        if professor["id"] == id_professor:
-            dicie["professores"].remove(professor)
-            return jsonify({"mensagem": "Professor removido com sucesso"})
-    return jsonify({"error": "Professor não encontrado"}), 404
-
-
-#TURMAS
-#exibir turmas
-@app.route('/turmas', methods=['GET'])
-def listar_turmas():
-    return jsonify(dicie["turma"])
-
-#exibir turmas pelo id
-@app.route('/turmas/<int:id_turma>', methods=['GET'])
-def obter_turma(id_turma):
-    for turma in dicie["turma"]:
-        if turma["id"] == id_turma:
-            return jsonify(turma)
-
-#criar turmas
-@app.route('/turmas', methods=['POST'])
-def criar_turma():
-    dados = request.json
-    nova_turma = {"id": dados["id"], "nome": dados["nome"]}
-    dicie["turma"].append(nova_turma)
-    return jsonify(nova_turma), 201
-
-#atualizar turmas
-@app.route('/turmas/<int:id_turma>', methods=['PUT'])
-def atualizar_turma(id_turma):
-    for turma in dicie["turma"]:
-        if turma["id"] ==  id_turma:
-            dados = request.json
-            turma["nome"] = dados.get("nome", turma["nome"])
-            return jsonify(turma)
-    return jsonify({"error": "Turma não encontrado"}), 404
-
-#deletar turma
-@app.route('/turmas/<int:id_turma>', methods=['DELETE'])
-def excluir_turma(id_turma):
-    for turma in dicie["turma"]:
-        if turma["id"] == id_turma:
-            dicie["turma"].remove(turma)
-            return jsonify({"mensagem": "Turma removida com sucesso"})
-    return jsonify({"error": "Turma não encontrada"}), 404
-
-
-# ---------------- ALUNOS ----------------
+# ---------------- BLUEPRINTS ----------------
 app.register_blueprint(alunos_blueprint)
-
-# ---------------- PROF -------------------
 app.register_blueprint(professores_blueprint)
-
-# --------------- TURMAS -----------------
 app.register_blueprint(turmas_blueprint)
 
-
-# ---------------- LOGIN ----------------
+# LOGIN
 app.register_blueprint(login_blueprint)
 
-#roda essa api 
+# RODA A API
 if __name__ == '__main__':
     app.run(debug=True)
-
-
